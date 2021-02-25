18e4a634ef526b713351d25cae8797e8fa842e26
RadhikaRJ <radhika7002@gmail.com>
2021-02-25 20:08:40 +0530

    Basic todoInput

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from "react";
 function App() {
+  const todoList=['buy milk','buy fruits'];
+  const[done, setDone]=useState(false);
+  
+  
   return (
     <div className="App">
-      
+      <label>To do: </label>
+      <input  id="todoInput" type="text"></input><span> </span>
+      <button >Add</button>
+      <div>Your To Do List: </div>
+     
+       
     </div>
   );
 }
