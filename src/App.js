184d019d2ea486e38b3f854915bfcd5fe127965a
RadhikaRJ <radhika7002@gmail.com>
2021-02-25 20:29:20 +0530

    feat: Add & clearAll items from todo list

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
 import {useState} from "react";
 function App() {
-  const todoList=['buy milk','buy fruits'];
-  const[done, setDone]=useState(false);
+  const [todoList,setTodoList]=useState(['buy milk','buy fruits']);
   
   
+  const[currentInputValue,setCurrentInputValue]=useState("");
+  
+
+
   return (
     <div className="App">
       <label>To do: </label>
-      <input  id="todoInput" type="text"></input><span> </span>
-      <button >Add</button>
+      <input onChange={(e)=>setCurrentInputValue(e.target.value)} type="text"></input><span> </span>
+      <button onClick={()=>setTodoList(todoList.concat(currentInputValue))} >Add</button><span> </span>
+      <button onClick={()=>setTodoList([])}>Clear</button>
       <div>Your To Do List: </div>
      {
        todoList.map((item)=>(
-         <div><input type="checkbox"></input>{item}
+         <div><input type="checkbox" value=""></input>{item}
          </div>
 
        ))
